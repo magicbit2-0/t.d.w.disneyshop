f25d5d594d40d9ecb8d71d2d86381d2e57efd678
Martina99188 <martina_nolletti@yahoo.it>
2021-01-14 16:40:41 +0100

    sdfghj

--- Sito Disney/dtml/inserimento_martina.sql
+++ Sito Disney/dtml/inserimento_martina.sql
@@ -8,3 +8,12 @@ INSERT INTO `disneydb`.`gruppo_utente` (`id`, `utente_id`, `gruppo_id`) VALUES (
 
 INSERT INTO `disneydb`.`gruppo` (`id`, `tipologia utente`) VALUES ('1','cliente');
 INSERT INTO `disneydb`.`gruppo` (`id`, `tipologia utente`) VALUES ('1','amministratore');
+
+INSERT INTO `disneydb`.`indirizzo_spedizione` (`id`, `nome`, `cognome`, `indirizzo1`, `paese`, `regione`, `citta`, `cap`, `telefono`, `utente_id`) VALUES ('1', 'Martina', 'Nolletti', 'Piazza dei Longobardi n°6', 'Italia', 'Abruzzo', 'L\'Aquila', '67049', '3459319859', '2');
+INSERT INTO `disneydb`.`indirizzo_spedizione` (`id`, `nome`, `cognome`, `indirizzo1`, `paese`, `regione`, `citta`, `cap`, `telefono`, `utente_id`) VALUES ('2', 'Enrico', 'Simone', 'Via Sulmona n°3', 'Italia', 'Abruzzo', 'Sulmona', '67039', '3456723456', '1');
+INSERT INTO `disneydb`.`indirizzo_spedizione` (`id`, `nome`, `cognome`, `indirizzo1`, `paese`, `regione`, `citta`, `cap`, `telefono`, `utente_id`) VALUES ('3', 'Michele', 'Intrevado', 'Via Carlo Montari n°11', 'Italia', 'Abruzzo', 'Pescara', '65015', '348974239', '3');
+
+INSERT INTO `disneydb`.`metodo_pagamento` (`id`, `numero_carta`, `cvv`, `data_scadenza`, `nome_sulla_carta`, `utente_id`) VALUES ('1', '123456789101112131', '456', '08/24', 'Enrico Simone Adamelli', '1');
+INSERT INTO `disneydb`.`metodo_pagamento` (`id`, `numero_carta`, `cvv`, `data_scadenza`, `nome_sulla_carta`, `utente_id`) VALUES ('2', '8987654321456743', '123', '09/27', 'Martina Nolletti', '2');
+INSERT INTO `disneydb`.`metodo_pagamento` (`id`, `numero_carta`, `cvv`, `data_scadenza`, `nome_sulla_carta`, `utente_id`) VALUES ('3', '2312321378965437', '789', '03/22', 'Michele Intrevado', '3');
+
