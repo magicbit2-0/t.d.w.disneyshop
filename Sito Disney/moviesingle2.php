daf94a734af40b4835551ddbd19c73290d96ff59
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-01-14 16:04:27 +0100

    vreiuhgro

--- Sito Disney/moviesingle2.php
+++ Sito Disney/moviesingle2.php
@@ -2,7 +2,7 @@
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
-$main=new Template("dtml/bloggrid.html");
+$main=new Template("dtml/moviesingle2.html");
 
 $main->close();
 ?>
