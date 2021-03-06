192be6b128f29d1c0eb3b4f639122a9f8d5b257f
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-03-06 12:24:38 +0100

    rghjkl

---
+++ Sito Disney/trailer.php
@@ -0,0 +1,12 @@
+<?php
+
+require "include/dbms.inc.php";
+
+$result = $mysqli->query("select trailer from articolo where id={$_REQUEST['id']}");
+
+$data = $result->fetch_assoc();
+
+header("Content-Type: video/mp4");
+echo $data['locandina'];
+
+?>
