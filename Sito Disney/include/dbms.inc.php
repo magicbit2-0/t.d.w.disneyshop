1f78f9e670c3e94e07e131f4e300863f7ae5fca5
Martina99188 <martina_nolletti@yahoo.it>
2021-03-26 18:22:57 +0100

    .

--- Sito Disney/include/dbms.inc.php
+++ Sito Disney/include/dbms.inc.php
@@ -1,7 +1,9 @@
 <?php
-$mysqli = new mysqli('127.0.0.1','root', '','disneydb');
-if($mysqli -> connect_errno){
-    printf("connection failed: %s\n", $mysqli -> connect_error);
-    exit();
-}
+
+    $mysqli = new mysqli('127.0.0.1','root', '','disneydb');
+
+    if($mysqli -> connect_errno){
+        printf("connection failed: %s\n", $mysqli -> connect_error);
+        exit();
+    }
 ?>
--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -1,19 +1,18 @@
 <?php
+
     require "include/dbms.inc.php";
     require "include/template2.inc.php";
 
-    $main=new Template("dtml/index.html");
-
-    $main->setContent("titolo", "Titolo film");
+    $main = new Template("dtml/index.html");
 
     $result = $mysqli -> query("SELECT * FROM articolo ORDER BY titolo ASC");
 
     while ($data = $result -> fetch_assoc()){
-        $main->setContent("titolo_film", $data['titolo']);
-        $main->setContent("categoria_film", $data['categoria']);
+        $main->setContent("titolo_prodotto", $data['titolo']);
+        $main->setContent("categoria_prodotto", $data['categoria']);
+        $main->setContent("votazione_prodotto", $data['votazione']);
+        $main->setContent("immagine_prodotto", $data['locandina']);
     };
-
-    //exit;
 
     $main->close();
 ?>
