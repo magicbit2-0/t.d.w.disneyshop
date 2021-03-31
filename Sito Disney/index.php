eb743af3c255a4e8184fa0e27b517c1ae24bae82
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-03-31 19:40:16 +0200

    .

--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -1,17 +1,15 @@
 <?php
-
-    require "include/dbms.inc.php";
     require "include/template2.inc.php";
+    require "include/dbms.inc.php";
 
     $main = new Template("dtml/index.html");
 
-    $result = $mysqli -> query("SELECT * FROM articolo ORDER BY titolo ASC");
-
+    $result = $mysqli -> query("SELECT titolo, categoria, votazione FROM articolo ORDER BY titolo ASC");
     while ($data = $result -> fetch_assoc()){
         $main->setContent("titolo_prodotto", $data['titolo']);
-        $main->setContent("categoria_prodotto", $data['categoria']);
-        $main->setContent("votazione_prodotto", $data['votazione']);
-        $main->setContent("immagine_prodotto", $data['locandina']);
+        $main->setContent("categoria_prodotto",$data['categoria'] );
+        $main->setContent("votazione_prodotto",$data['votazione']);
+        $main->setContent("immagine_prodotto",$data['locandina'] );
     };
 
     $main->close();
