4ade6cb5434ff7c92006a20c42ae8819f10fccef
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-01 18:38:31 +0200

    .

--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -4,14 +4,13 @@
 
     $main = new Template("dtml/index.html");
 
-    $result = $mysqli -> query("SELECT id, titolo, categoria, votazione FROM articolo ORDER BY titolo ASC");
+    $result = $mysqli -> query("SELECT titolo, categoria, votazione, locandina FROM articolo;");
     while ($data = $result -> fetch_assoc()){
         $main->setContent("titolo_prodotto", $data['titolo']);
-        $main->setContent("categoria_prodotto",$data['categoria'] );
+        $main->setContent("categoria_prodotto",$data['categoria']);
         $main->setContent("votazione_prodotto",$data['votazione']);
         //$main->setContent("immagine_prodotto",$data['locandina'] );
 
-    };
+    }
 
     $main->close();
-?>
