2422b7635ff38f25273ec15dd124e646be050f6a
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-01 19:12:22 +0200

    seryhvftyj

--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -4,7 +4,7 @@
 
     $main = new Template("dtml/index.html");
 
-    $result = $mysqli -> query("SELECT titolo, categoria, votazione, locandina FROM articolo;");
+    $result = $mysqli -> query("SELECT titolo, categoria, votazione FROM articolo;");
     while ($data = $result -> fetch_assoc()){
         $main->setContent("titolo_prodotto", $data['titolo']);
         $main->setContent("categoria_prodotto",$data['categoria']);
