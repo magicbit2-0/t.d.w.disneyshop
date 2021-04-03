855b24a1a7e8cb19b31db7c9e5d09f815b498439
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-03 17:03:41 +0200

    rip

--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -10,11 +10,11 @@
         $result = $mysqli->query("SELECT titolo, categoria, votazione FROM articolo ");
 
     while ($data = $result->fetch_assoc()) {
-        $main->setContent("titolo_prodotto", $data['titolo']);
+        $main->setContent("titolo_prodotto", "pippo");
         $main->setContent("categoria_prodotto", $data['categoria']);
         $main->setContent("votazione_prodotto", $data['votazione']);
         //$main->setContent("immagine_prodotto",$data['locandina'] );
     }
-}
+    }
     $main->close();
 ?>
--- Sito Disney/moviegrid.php
+++ Sito Disney/moviegrid.php
@@ -1,10 +1,19 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
 $main=new Template("dtml/moviegrid.html");
 
-$film = $mysqli->query("select  from articolo");
+$result = $mysqli->query("SELECT titolo, categoria, votazione FROM articolo ");
+
+
+while ($data = $result->fetch_assoc()) {
+    $main->setContent("titolo_prodotto", "bbbbbbbbb");
+    $main->setContent("categoria_prodotto", $data['categoria']);
+    $main->setContent("votazione_prodotto", $data['votazione']);
+    //$main->setContent("immagine_prodotto",$data['locandina'] );
+}
 
 $main->close();
 ?>
