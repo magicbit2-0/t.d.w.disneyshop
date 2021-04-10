2e9c6fe707d0c48e6d6f8db568156035a8cc7313
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-10 13:00:45 +0200

    .

--- Sito Disney/index.php
+++ Sito Disney/index.php
@@ -6,7 +6,11 @@
     $main = new Template("dtml/index.html");
 
     if (isset($mysqli)) {
-        $result = $mysqli->query("(SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Film Disney' order by data_uscita desc limit 2) union
+        $result = $mysqli->query("(SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Film Disney' order by data_uscita desc limit 1) union
+        (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cartone Pixar' order by data_uscita desc limit 1) union
+        (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cartone Disney' order by data_uscita desc limit 1) union
+        (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cortometraggi Pixar' order by data_uscita desc limit 1) union
+        (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Film Disney' order by data_uscita desc limit 2) union
         (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cartone Pixar' order by data_uscita desc limit 2) union
         (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cartone Disney' order by data_uscita desc limit 2) union
         (SELECT titolo, categoria, votazione, locandina FROM articolo where categoria = 'Cortometraggi Pixar' order by data_uscita desc limit 2);");
@@ -15,13 +19,10 @@
         $main->setContent("titolo_prodotto", $data['titolo']);
         $main->setContent("categoria_prodotto", $data['categoria']);
         $main->setContent("votazione_prodotto", $data['votazione']);
-        header("Content-Type: image/jpeg");
-        $main->setContent("locandina_prodotto", $data['locandina']);
+       // header("Content-Type: image/jpeg");
+        //$main->setContent("locandina_prodotto", $data['locandina']);
 
     }
-//SELECT titolo, categoria, votazione FROM articolo where categoria = "Film Disney" order by data_uscita desc limit 2 ;
-//SELECT titolo, categoria, votazione FROM articolo where categoria = "Cartone Pixar" order by data_uscita desc limit 2 ;
-//SELECT titolo, categoria, votazione FROM articolo where categoria = "Cortometraggi Pixar" order by data_uscita desc limit 2 ;
     }
 
     $main->close();
