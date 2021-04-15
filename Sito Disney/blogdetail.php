19e918e9f9a2f7649d7f06a085ea88458e745a21
Martina99188 <martina_nolletti@yahoo.it>
2021-04-15 18:14:26 +0200

    .

--- Sito Disney/blogdetail.php
+++ Sito Disney/blogdetail.php
@@ -1,22 +1,22 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE);
-require "include/dbms.inc.php";
-require "include/template2.inc.php";
+    error_reporting(E_ALL & ~E_NOTICE);
+    require "include/dbms.inc.php";
+    require "include/template2.inc.php";
 
-$main=new Template("dtml/index.html");
-$body=new Template("dtml/blog_detail.html");
+    $main=new Template("dtml/index.html");
+    $body=new Template("dtml/blog_detail.html");
 
-if (isset($mysqli)) {
-    $result = $mysqli->query("(SELECT id as idImgNotizia, titolo, data_pubblicazione, descrizione FROM notizia");
+    if (isset($mysqli)) {
+        $result = $mysqli->query("(SELECT id, titolo, data_pubblicazione, descrizione FROM notizia");
 
-    while ($data = $result->fetch_assoc()){
-        $body->setContent("titolo_notizia", $data['titolo']);
-        $body->setContent("data_notizia", $data['data_pubblicazione']);
-        $body->setContent("descrizione_notizia", $data['descrizione']);
-        $body->setContent("idImgNotizia", $data['idImgNotizia']);
+        while ($data = $result->fetch_assoc()){
+            $body->setContent("titolo_notizia", $data['titolo']);
+            $body->setContent("data_notizia", $data['data_pubblicazione']);
+            $body->setContent("descrizione_notizia", $data['descrizione']);
+            $body->setContent("idImgNotizia", $data['id']);
+        }
     }
-}
 
-$main->setContent("body", $body->get());
-$main->close();
+    $main->setContent("body", $body->get());
+    $main->close();
 ?>
