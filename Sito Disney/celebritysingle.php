80419d3073b24e0c204981a4fc744e87d361f5c2
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-15 20:00:24 +0200

    vcftyujn

--- Sito Disney/celebritysingle.php
+++ Sito Disney/celebritysingle.php
@@ -1,8 +1,10 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
 $main=new Template("dtml/celebritysingle.html");
+$body=new Template("dtml/celebrity_single.html");
 
 $main->close();
 ?>
