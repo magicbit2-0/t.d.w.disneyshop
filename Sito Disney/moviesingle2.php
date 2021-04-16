7f5ba7f6d8717069fb89bd107f34a046ee9e6da7
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-16 17:35:41 +0200

    .

--- Sito Disney/moviesingle2.php
+++ Sito Disney/moviesingle2.php
@@ -1,8 +1,20 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
-$main=new Template("dtml/moviesingle2.html");
+$main=new Template("dtml/index.html");
+$body=new Template("dtml/movie_single2.html");
 
+if (isset($mysqli)) {
+    $result = $mysqli->query("select id as idImg, titolo, data_uscita, durata, trama, votazione, prezzo, categoria from articolo where id = {$_GET['id']}");
+    $data = $result->fetch_assoc();
+
+    foreach ($data as $key => $value){
+        $body->setContent($key,$value);
+    }
+}
+
+$main->setContent("body", $body->get());
 $main->close();
 ?>
