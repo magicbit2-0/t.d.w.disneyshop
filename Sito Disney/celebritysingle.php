e41f1ca0b332434752fdc7c64bd3877c9e49b06f
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-17 17:06:17 +0200

    attore

--- Sito Disney/celebritysingle.php
+++ Sito Disney/celebritysingle.php
@@ -3,8 +3,20 @@ error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
-$main=new Template("dtml/celebritysingle.html");
+$main=new Template("dtml/index.html");
 $body=new Template("dtml/celebrity_single.html");
 
+if (isset($mysqli)) {
+
+    $result = $mysqli->query("select id as idImgAttore, nome, cognome, anno_nascita, eta, nazionalità, paese_nascita, biografia, foto");
+    $data = $result->fetch_assoc();
+    foreach($data as $key => $value){
+        $body->setContent($key,$value);
+    }
+
+
+}
+
+$main->setContent("body", $body->get());
 $main->close();
 ?>
