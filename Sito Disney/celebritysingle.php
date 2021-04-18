73da1a7a8ac47aaa2f0cbd6f9280c8522c7f9f8c
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-18 14:46:51 +0200

    ...

--- Sito Disney/celebritysingle.php
+++ Sito Disney/celebritysingle.php
@@ -8,13 +8,26 @@ $body=new Template("dtml/celebrity_single.html");
 
 if (isset($mysqli)) {
 
-    $result = $mysqli->query("select id as idImgAttore, nome, cognome, anno_nascita, eta, nazionalità, paese_nascita, biografia, foto");
+    $result = $mysqli->query("select id as idImgAttore, nome, cognome, anno_nascita, eta, nazionalità, paese_nascita, biografia, foto from regia where id = {$_GET['id']}");
     $data = $result->fetch_assoc();
     foreach($data as $key => $value){
         $body->setContent($key,$value);
     }
 
+    $result1 = $mysqli->query("select concat(nome,' ',cognome) as nomeCognome from regia where id = {$_GET['id']}");
+    while ($data1 = $result1->fetch_assoc()){
+        $body->setContent("nomeCognomeAttore",$data1['nomeCognome']);
+    }
 
+    $result2 = $mysqli->query("select nome, cognome, anno_nascita, eta, nazionalità from regia where id = {$_GET['id']};");
+    while ($data2 = $result2->fetch_assoc()){
+        $body->setContent("nome",$data2['nome']);
+        $body->setContent("cognome",$data2['cognome']);
+        $body->setContent("annoNascita",$data2['anno_nascita']);
+        $body->setContent("eta",$data2['eta']);
+        $body->setContent("nazionalita",$data2['nazionalità']);
+
+    }
 }
 
 $main->setContent("body", $body->get());
