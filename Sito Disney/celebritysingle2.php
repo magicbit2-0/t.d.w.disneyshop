6e4975e98de8136d14092c8928bfb5f9f3c8d11e
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-20 20:17:51 +0200

    personaggio

--- Sito Disney/celebritysingle2.php
+++ Sito Disney/celebritysingle2.php
@@ -1,8 +1,25 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
-$main=new Template("dtml/celebritysingle2.html");
+$main=new Template("dtml/index.html");
+$body=new Template("dtml/celebrity_single2.html");
 
+if(isset($mysqli)){
+
+    $result = $mysqli->query("select id as id_personaggio, nome, descrizione, data_nascita, foto from personaggio where id = {$_GET['id']}");
+    $data = $result->fetch_assoc();
+    foreach($data as $key => $value){
+        $body->setContent($key,$value);
+    }
+    
+    /*$result = $mysqli->query("");
+    while ($data = $result->fetch_assoc()){
+        $body->setContent("", $data['']);
+    }*/
+}
+
+$main->setContent("body", $body->get());
 $main->close();
 ?>
