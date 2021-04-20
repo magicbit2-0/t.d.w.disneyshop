29e546801044b1668288ab6c55dcfe0dabcf8081
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-20 18:37:48 +0200

    film

--- Sito Disney/celebritysingle.php
+++ Sito Disney/celebritysingle.php
@@ -14,6 +14,15 @@ if (isset($mysqli)) {
         $body->setContent($key,$value);
     }
 
+
+    $result1 = $mysqli->query("select distinct a.id as idfilm, a.titolo as titolofilm, a.data_uscita as datafilm, ps.nome as nome_protagonista from regia r join backstage_articolo b on (r.id = b.regia_id) join articolo a on (b.articolo_id = a.id) join personaggio_articolo pa on pa.articolo_id = a.id join personaggio ps on ps.id = pa.personaggio_id where r.id = {$_GET['id']} group by a.id");
+    while ($data1 = $result1->fetch_assoc()){
+        $body->setContent("idfilm", $data1['idfilm']);
+        $body->setContent("titolofilm", $data1['titolofilm']);
+        $body->setContent("datafilm", $data1['datafilm']);
+        $body->setContent("nome_protagonista", $data1['nome_protagonista']);
+    }
+
 }
 
 $main->setContent("body", $body->get());
