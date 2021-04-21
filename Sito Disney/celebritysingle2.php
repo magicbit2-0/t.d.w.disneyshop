1b0906b62af493c93f05a859822a3acc66f8ea1c
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-21 19:30:50 +0200

    .

--- Sito Disney/celebritysingle2.php
+++ Sito Disney/celebritysingle2.php
@@ -8,16 +8,25 @@ $body=new Template("dtml/celebrity_single2.html");
 
 if(isset($mysqli)){
 
-    $result = $mysqli->query("select id as id_personaggio, nome, descrizione, data_nascita, foto from personaggio where id = {$_GET['id']}");
+    $result = $mysqli->query("select id as id_personaggio, nome, descrizione, data_nascita from personaggio where id = {$_GET['id']}");
     $data = $result->fetch_assoc();
     foreach($data as $key => $value){
         $body->setContent($key,$value);
     }
-    
-    /*$result = $mysqli->query("");
+
+    $result = $mysqli-> query("SELECT p.testo as parola_chiave from parola_chiave_personaggio pc join parola_chiave p on pc.parola_chiave_id = p.id join personaggio d on d.id = pc.personaggio_id where d.id = {$_GET['id']}");
     while ($data = $result->fetch_assoc()){
-        $body->setContent("", $data['']);
-    }*/
+        $body->setContent("parola_chiave", $data['parola_chiave']);
+    }
+
+    $result = $mysqli->query("select a.id, a.titolo, a.data_uscita from personaggio_articolo pa join articolo a on a.id = pa.articolo_id join personaggio p on p.id = pa.personaggio_id where p.id = {$_GET['id']}");
+    while ($data = $result->fetch_assoc()){
+        $body->setContent("idcartone", $data['id']);
+        $body->setContent("titolo_film", $data['titolo']);
+        $body->setContent("titolo_cartone", $data['titolo']);
+        $body->setContent("data_cartone", $data['data_uscita']);
+
+    }
 }
 
 $main->setContent("body", $body->get());
