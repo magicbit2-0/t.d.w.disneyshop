3261f4991431076449da9cf6370f77c46652fcf1
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-27 19:05:08 +0200

    ..

--- Sito Disney/include/auth.php
+++ Sito Disney/include/auth.php
@@ -1,23 +1,5 @@
 <?php
-
-    class Auth{
-        function doLogin(){
-            
-            global $mysqli;
-            $result = $mysqli->query("select * from users where username = {$_POST['username']} and password = md5({$_POST['password']})");
-            
-            if(!$result){
-                echo "Errore Autenticazione";
-                exit;
-            }
-
-            if($result->num_rows == 1){
-                Header("Location: login.php?error");
-                exit;
-               
-            }
-        }
-
-    }
-    Auth::doLogin();
+error_reporting(E_ALL & ~E_NOTICE);
+require "dbms.inc.php";
+require "template2.inc.php";
 ?>
