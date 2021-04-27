58749095c693b168092370e1ae9b5854f8ce20bd
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-04-27 17:05:57 +0200

    robe a caso

---
+++ Sito Disney/login.php
@@ -0,0 +1,13 @@
+<?php
+    error_reporting(E_ALL & ~E_NOTICE);
+    require "include/dbms.inc.php";
+    require "include/template2.inc.php";
+
+    $main = new Template("dtml/index.html");
+    //$body = new Template("dtml/homepage.html");
+
+    
+
+    //$main->setContent("body", $body->get());
+    $main->close();
+?>
