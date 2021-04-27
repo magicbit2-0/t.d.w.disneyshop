a8f1507de618d9e497dd2fe08611d1929f6a6a34
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-27 18:33:12 +0200

    .

--- Sito Disney/userfavoritegrid.php
+++ Sito Disney/userfavoritegrid.php
@@ -1,9 +1,10 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
 $main=new Template("dtml/index2.html");
-$body=new Template("dtml/shop_page.html");
+$body=new Template("dtml/user_favoritegrid.html");
 
 $main->setContent("body", $body->get());
 $main->close();
--- Sito Disney/userprofile.php
+++ Sito Disney/userprofile.php
@@ -3,8 +3,9 @@ error_reporting(E_ALL & ~E_NOTICE);
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 
-$main=new Template("dtml/index.html");
+$main=new Template("dtml/index2.html");
 $body=new Template("dtml/user_profile.html");
 
+$main->setContent("body", $body->get());
 $main->close();
 ?>
