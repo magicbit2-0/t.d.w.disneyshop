1b2b405573a24041b4ab2aac731f3ed672b2ef32
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-29 18:09:17 +0200

    .

--- Sito Disney/blogdetail.php
+++ Sito Disney/blogdetail.php
@@ -21,7 +21,6 @@
         $result = $mysqli->query("SELECT u.id, u.nome, u.cognome, c.data, c.testo FROM utente u join commento c on u.id = c.utente_id join notizia n on c.notizia_id = n.id WHERE n.id={$_GET['id']}");
 
         while ($data1 = $result->fetch_assoc()) {
-            $body->setContent("pagina_utente", 'userprofile.php?id=<[idUtente]>');
             $body->setContent("nome_utente", $data1['nome']);
             $body->setContent("data_commento", $data1['data']);
             $body->setContent("testo_commento", $data1['testo']);
@@ -30,19 +29,19 @@
         $result = $mysqli->query("SELECT id as idNotizia1, titolo FROM notizia WHERE categoria='Cartone Disney' limit 1");
         while ($data2 = $result->fetch_assoc()) {
             $body->setContent("notizia1", $data2['titolo']);
-            $body->setContent("pagina_notizia1", 'blogdetail.php?id=<[idNotizia1]>');
+            $body->setContent("pagina_notizia1", 'blogdetail.php?id='.$data2['idNotizia1']);
         }
 
         $result = $mysqli->query("SELECT id as idNotizia2, titolo FROM notizia WHERE categoria='Film Disney' limit 1");
         while ($data3 = $result->fetch_assoc()) {
             $body->setContent("notizia2", $data3['titolo']);
-            $body->setContent("pagina_notizia2", 'blogdetail.php?id=<[idNotizia2]>');
+            $body->setContent("pagina_notizia2", 'blogdetail.php?id='.$data3['idNotizia2']);
         }
 
         $result = $mysqli->query("SELECT id as idNotizia3, titolo FROM notizia order by data_pubblicazione desc limit 1");
         while ($data4 = $result->fetch_assoc()) {
             $body->setContent("notizia3", $data4['titolo']);
-            $body->setContent("pagina_notizia3", 'blogdetail.php?id=<[idNotizia3]>');
+            $body->setContent("pagina_notizia3", 'blogdetail.php?id='.$data4['idNotizia3']);
         }
     }
 
