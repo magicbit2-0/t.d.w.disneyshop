2742c033e5d5ca393a64e930b6ab4136e30dc70c
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-04-30 20:57:41 +0200

    .

---
+++ Sito Disney/profileAdmin.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+require "include/dbms.inc.php";
+require "include/template2.inc.php";
+//require "bottonChange.php";
+
+//$main=new Template("dtml/ADMIN/admin.html");
+$main=new Template("dtml/ADMIN/pages/examples/admin_body.html");
+$body=new Template("dtml/ADMIN/pages/examples/profile.html");
+if (isset($mysqli)) {
+
+    /*$result = $mysqli->query("select id as idImgAttore, nome, cognome, anno_nascita, eta, nazionalità as nazionalita, paese_nascita, biografia, foto from regia where id = {$_GET['id']}");
+    $data = $result->fetch_assoc();
+    while ($data = $result->fetch_assoc()){
+        $body->setContent("parola_chiave", $data['testo']);
+    }*/
+}
+$main->setContent("body_admin", $body->get());
+$main->close();
+?>
