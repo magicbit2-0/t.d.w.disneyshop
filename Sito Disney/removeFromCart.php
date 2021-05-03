9ad78fda8b2c52f81af6000fe21d85af1ea5e4d6
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-05-03 17:38:41 +0200

    vjhghj

---
+++ Sito Disney/removeFromCart.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require "include/dbms.inc.php";
+echo "qui " .$_GET['id'];
+
+    for ($i=0;$i<count($_SESSION['articoli']);$i++){
+        if ($_SESSION['articoli'][$i] == $_GET['id']){
+            unset($_SESSION['articoli'][$i]);
+        }
+    }
+
+    header('Location: http://localhost/t.d.w.disneyshop/Sito%20Disney/shoppage.php');
+?>
