a241572ba612528e7f0b62d51113244e47310f44
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-05-06 20:00:13 +0200

    avatar

--- Sito Disney/userprofile.php
+++ Sito Disney/userprofile.php
@@ -39,14 +39,48 @@ if (isset($mysqli)){
 
     foreach ($data as $key => $value){
         $body->setContent($key,$value);
-        $body->setContent("foto_avatar",'<a href="#"><img src="imgAvatar.php?id='.$data['avatar_id'] .'" alt="" style="background-color:'.$backgroundcolor.';border-radius: 50%;width: 120px;height: 120px;"><br></a>'
-);
+        $body->setContent("foto_avatar",'<a href="#"><img src="imgAvatar.php?id='.$data['avatar_id'] .'" alt="" style="background-color:'.$backgroundcolor.';border-radius: 50%;width: 120px;height: 120px;"><br></a>');
     }
     /*
     while ($data = $result->fetch_assoc()) {
         $body->setContent("nome_attore", $data['nomeT']);
         $body->setContent("idImgAttore", $data['id']);
     }*/
+
+    $result = $mysqli->query("select id from avatar where id not in (select avatar_id from utente where id = {$_SESSION['idUtente']});");
+    while ($data = $result->fetch_assoc()) {
+        switch($data['id']){
+            case '1':
+                $backgroundcolor = 'lightpink';
+                break;
+            case '2':
+                $backgroundcolor = '#d3c24b';
+                break;
+            case '3':
+                $backgroundcolor = '#d584cf';
+                break;
+            case '4':
+                $backgroundcolor = 'cadetblue';
+                break;
+            case '5':
+                $backgroundcolor = '#50b666';
+                break;
+            case '6':
+                $backgroundcolor = '#6792c7';
+                break;
+            case '7':
+                $backgroundcolor = '#e86c6c';
+                break;
+            default:
+                $backgroundcolor = 'aliceblue';
+                break;
+        }
+        $body->setContent("avatar_disponibili",'<a><img src="imgAvatar.php?id='.$data['id'] .'" alt="" style="background-color:'.$backgroundcolor.';
+                                                border-radius: 50%;width: 120px;height: 120px; margin-bottom: 10px;"><br>
+                                                <input type="radio" name="radioAvatar" value="'.$data['id'].'"> Scegli Questo Avatar</a>');
+        $body->setContent("idAvatar",$data['id']);
+
+    }
 }
 
 $main->setContent("body", $body->get());
