d0a533e418e2dbd5570c611afd05925f925da185
micheleintrevado <michele.intrevado.pc@gmail.com>
2021-05-10 13:30:51 +0200

    jbhj

---
+++ Sito Disney/updateProfile.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+error_reporting(E_ALL & ~E_NOTICE);
+require "include/dbms.inc.php";
+require "include/template2.inc.php";
+session_start();
+error_reporting(E_ALL & ~E_NOTICE);
+require "include/dbms.inc.php";
+require "include/template2.inc.php";
+require "bottonChange.php";
+
+//query aggiornamento dati profilo utente 
+$mysqli->query("");
+
+?>
