5215aa75045f7897c2e7d3018332eee80b253b02
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-06-03 15:38:01 +0200

    ..

--- Sito Disney/celebritysingle.php
+++ Sito Disney/celebritysingle.php
@@ -17,19 +17,51 @@ if (isset($mysqli)) {
     }
 
 
-    $result1 = $mysqli->query("select distinct a.id as idfilm, a.titolo as titolofilm, a.data_uscita as datafilm, a.categoria, ps.nome as nome_protagonista from regia r join backstage_articolo b on (r.id = b.regia_id) join articolo a on (b.articolo_id = a.id) join personaggio_articolo pa on pa.articolo_id = a.id join personaggio ps on ps.id = pa.personaggio_id where r.id = {$_GET['id']} group by a.id");
+    $result = $mysqli->query("select distinct a.id as idfilm, a.titolo as titolofilm, a.data_uscita as datafilm, a.categoria from regia r 
+                                        join backstage_articolo b on r.id = b.regia_id 
+                                        join articolo a on b.articolo_id = a.id 
+                                        where r.id = {$_GET['id']} limit 3");
+    if(mysqli_num_rows($result) > 0) {
+        while ($data1 = $result->fetch_assoc()) {
+            $body->setContent("film_correlato", '<div class="cast-it">
+                                                            <div class="cast-left cebleb-film">
+                                                                <img src=\'img.php?id='.$data1['idfilm'].'\' alt="immagine film non trovata" width="200px">
+                                                                <div>
+                                                                    <a href=\'moviesingle.php?id='.$data1['idfilm'].'\'>'.$data1['titolofilm'].'</a>
+                                                                    <p style="width: auto;">'.$data1['datafilm'].'</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>');
+
+            $body->setContent("idfilm", $data1['idfilm']);
+            $body->setContent("idfilm1", $data1['idfilm']);
+            $body->setContent("titolofilm", $data1['titolofilm']);
+            $body->setContent("titolofilm1", $data1['titolofilm']);
+            $body->setContent("titolo_film2", $data1['titolofilm']);
+            $body->setContent("datafilm", $data1['datafilm']);
+            $body->setContent("datafilm1", $data1['datafilm']);
+            $body->setContent("categoria_film", $data1['categoria']);
+        }
+    } else {
+        $body->setContent("film_correlato", "<div><h2 style='color:#d36b6b'> Non sono ancora stati trovati film correlati a questo vip </h2></div>");
+        $body->setContent("film_correlati2", "<div><h2 style='color:#d36b6b'> Non sono ancora stati trovati film correlati a questo vip </h2></div>");
+    }
+
+    $result1 = $mysqli->query("select distinct a.id as idfilm, a.titolo as titolofilm, a.data_uscita as datafilm, a.categoria from regia r 
+                                        join backstage_articolo b on r.id = b.regia_id 
+                                        join articolo a on b.articolo_id = a.id 
+                                        where r.id = {$_GET['id']}");
     while ($data1 = $result1->fetch_assoc()){
-        $numero_filmografia++;
-        $body->setContent("idfilm", $data1['idfilm']);
-        $body->setContent("idfilm1", $data1['idfilm']);
-        $body->setContent("titolofilm", $data1['titolofilm']);
-        $body->setContent("titolofilm1", $data1['titolofilm']);
-        $body->setContent("titolo_film2", $data1['titolofilm']);
-        $body->setContent("datafilm", $data1['datafilm']);
-        $body->setContent("datafilm1", $data1['datafilm']);
-        $body->setContent("nome_protagonista", $data1['nome_protagonista']);
-        $body->setContent("nome_protagonista1", $data1['nome_protagonista']);
-        $body->setContent("categoria_film", $data1['categoria']);
+        $body->setContent("film_correlati2", '<div class="cast-it">
+												<div class="cast-left cebleb-film">
+													<img src=\'img.php?id='.$data1['idfilm'].'\' alt="immagine film non trovata" style="width:150px;">
+													<div>
+														<a href=\'moviesingle.php?'.$data1['idfilm'].'\'>'.$data1['titolofilm'].'</a>
+														<p class="time" style="width: auto;">'.$data1['categoria'].'</p>
+													</div>
+												</div>
+												<p>'.$data1['datafilm'].'</p>
+											</div>');
     }
 
     $result = $mysqli->query("select p.testo as testo from parola_chiave_regia pc join regia r on r.id = pc.regia_id join parola_chiave p on p.id = pc.parola_chiave_id where r.id = {$_GET['id']}");
@@ -42,7 +74,7 @@ if (isset($mysqli)) {
         $body->setContent("parola_chiave_attore", $data['testo']);
     }
 
-    $body->setContent("numero",$numero_filmografia);
+    $body->setContent("numero",mysqli_num_rows($result1));
 }
 
 $main->setContent("body", $body->get());
