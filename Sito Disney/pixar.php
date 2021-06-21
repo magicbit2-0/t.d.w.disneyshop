83287365c92d7096889b4c0c93e0932855c15af8
Martina99188 <martina.nolletti@student.univaq.it>
2021-06-21 12:07:26 +0200

    brand

--- Sito Disney/pixar.php
+++ Sito Disney/pixar.php
@@ -5,6 +5,16 @@ require "include/template2.inc.php";
 require "bottonChange.php";
 
 $body=new Template("dtml/pixar.html");
+
+/*if (isset($mysqli)) {
+
+    $result = $mysqli->query("select id, nome, descrizione from brand where id = {$_GET['id']}");
+    $data = $result->fetch_assoc();
+    foreach($data as $key => $value){
+        $body->setContent($key,$value);
+    }
+}*/
+
 $main->setContent("body", $body->get());
 $main->close();
 ?>
